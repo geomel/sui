d5b02c159702514534516d44de977409b29f57a2
geomel <geomel@gmail.com>
2022-12-11 21:07:36 +0200

    feat: events for basic twitter functionality

--- dapps/Suitter/contracts/Solidity/(Sol)itter.sol
+++ dapps/Suitter/contracts/Solidity/(Sol)itter.sol
@@ -4,11 +4,15 @@ pragma solidity >=0.7.0 <0.9.0;
 
 /** 
  * @title (Sol)itter
- * @dev A basic twitter functionality with events
+ * @dev A basic twitter aka (Sol)eeter functionality with events
  */
 
  contract Solitter{
 
+    event NewSoleet(address recipient, uint soleetId); // event for new tweet
+    event EditSoleet(address recipient, uint soleetId); // event for edit tweets
+    event DeleteSoleet(uint tweetId, bool isDeleted); // event for deleting tweets
+    event ReSoleet(address recipient, uint soleetId); // event for retweet functionality
 
 
  }
