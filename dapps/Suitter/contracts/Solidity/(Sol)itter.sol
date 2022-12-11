cb66a2aa12a5724f0d32aaea820353bee9f9d4fe
geomel <geomel@gmail.com>
2022-12-11 23:24:15 +0200

    feat: create a new (Sol)eet

--- dapps/Suitter/contracts/Solidity/(Sol)itter.sol
+++ dapps/Suitter/contracts/Solidity/(Sol)itter.sol
@@ -11,8 +11,39 @@ pragma solidity >=0.7.0 <0.9.0;
 
     event NewSoleet(address recipient, uint soleetId); // event for new tweet
     event EditSoleet(address recipient, uint soleetId); // event for edit tweets
-    event DeleteSoleet(uint tweetId, bool isDeleted); // event for deleting tweets
+    event DeleteSoleet(uint soleetId, bool isDeleted); // event for deleting tweets
     event ReSoleet(address recipient, uint soleetId); // event for retweet functionality
+
+    // A Struct to hold a Tweet
+    struct Soleet{ 
+        uint256 id;
+        string soleetContent;
+        uint timestamp;
+        address owner;
+        uint likes;
+    }
+
+    mapping(uint => Soleet) public soleets;  // A map to store tweet IDs to Tweets
+
+    uint public soleetCounter; // Tweet counter and uniquw Soleet id
+
+    // Function to be called when creating a new Tweet 
+    function createSoleet(string memory text) public {
+        
+        soleetCounter++; // Soleet counter
+       
+        Soleet memory newSoleet = Soleet({  // New tweet
+        id: soleetCounter,
+        soleetContent: text,
+        timestamp: block.timestamp,
+        owner: msg.sender,
+        likes: 0
+        });
+
+        soleets[soleetCounter] = newSoleet; // Save newly created Soleet to map
+
+        emit NewSoleet(msg.sender, newSoleet.id); // Emit event when new Soleet is created
+  }
 
 
  }
