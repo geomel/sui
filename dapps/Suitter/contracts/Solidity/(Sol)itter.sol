e75e7b5ac76f9fd190690dc096ae600bccd1ee38
geomel <geomel@gmail.com>
2022-12-11 21:00:04 +0200

    refactor: changed (Sui)tter to (Sol)itter

--- dapps/Suitter/contracts/Solidity/(Sol)itter.sol
+++ dapps/Suitter/contracts/Solidity/(Sol)itter.sol
@@ -3,10 +3,12 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 /** 
- * @title (Sui)tter
+ * @title (Sol)itter
  * @dev A basic twitter functionality with events
  */
 
- contract Suitter{
+ contract Solitter{
+
+
 
  }
