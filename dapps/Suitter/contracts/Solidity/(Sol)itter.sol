aa7343703dc9885a0b485ed508d18736db2699f5
geomel <geomel@gmail.com>
2022-12-12 12:33:27 +0200

    feat: emit events when editing & deleting Soleets

--- dapps/Suitter/contracts/Solidity/(Sol)itter.sol
+++ dapps/Suitter/contracts/Solidity/(Sol)itter.sol
@@ -4,15 +4,18 @@ pragma solidity >=0.7.0 <0.9.0;
 
 /** 
  * @title (Sol)itter
- * @dev A basic twitter aka (Sol)eeter functionality with events
+ * @dev A basic twitter aka (Sol)itter functionality with events
  */
 
  contract Solitter{
 
     event NewSoleet(address recipient, uint soleetId); // event for new tweet
-    event EditSoleet(address recipient, uint soleetId); // event for edit tweets
+    event EditSoleet(address recipient, uint soleetId, string oldtext, string updatedText); // event for edit tweets
     event DeleteSoleet(uint soleetId, bool isDeleted); // event for deleting tweets
     event ReSoleet(address recipient, uint soleetId); // event for retweet functionality
+
+    // The maximum number of characters in a tweet
+    uint256 public maxSoleetChars = 280;
 
     // A Struct to hold a Tweet
     struct Soleet{ 
@@ -28,13 +31,18 @@ pragma solidity >=0.7.0 <0.9.0;
     uint public soleetCounter; // Tweet counter and uniquw Soleet id
 
     // Function to be called when creating a new Tweet 
-    function createSoleet(string memory text) public {
+    function createSoleet(string memory _text) public payable{
+
+        uint256 bytelength = bytes(_text).length;
+
+         // Check that the tweet is within the character limit
+        require(bytelength <= maxSoleetChars, "Tweet characters exceeded");
         
         soleetCounter++; // Soleet counter
        
-        Soleet memory newSoleet = Soleet({  // New tweet
+        Soleet memory newSoleet = Soleet({  // New tweet initianilization
         id: soleetCounter,
-        soleetContent: text,
+        soleetContent: _text,
         timestamp: block.timestamp,
         owner: msg.sender,
         likes: 0
@@ -46,28 +54,29 @@ pragma solidity >=0.7.0 <0.9.0;
   }
 
     // Function to be called when editing a new Tweet
-    function editSoleet(uint soleetId, string memory updatedText) public {
+    function editSoleet(uint soleetId, string memory _updatedText) public {
     
         Soleet storage soleet = soleets[soleetId]; // Returns the Tweet to edit from storage
 
         // Only the owner of the Tweet can edit it
         require(msg.sender == soleet.owner, "Only the owner of the (Sol)eet can edit it.");
+        string memory oldText = soleet.soleetContent;
+        soleet.soleetContent = _updatedText; // Update the Tweet and store it
 
-        soleet.soleetContent = updatedText; // Update the Tweet and store it
+        emit EditSoleet(msg.sender, soleetId, oldText, soleet.soleetContent); // Emit event when new Soleet is created
     }
 
-        // FFunction to be called when deleting a Tweet
+    // Function to be called when deleting a Tweet
     function deleteSoleet(uint soleetId) public {
        
         Soleet storage soleet = soleets[soleetId]; // Returns the Tweet to delete from storage
 
-        // Only the owner of the Tweet can edit it
+        // Only the owner of the Tweet can delete it
         require(msg.sender == soleet.owner, "Only the owner of the (Sol)eet can delete it.");
 
-        // Delete the tweet
-        delete soleets[soleetId];
+        delete soleets[soleetId];  // Delete the tweet
+
+        emit DeleteSoleet(soleetId, true);
 
     }
-
-
  }
